305d115689b48f23776cc6709fc03d9b6f67299e
nagendre1 <139125499+nagendre1@users.noreply.github.com>
2023-07-10 19:00:18 +0800

    Update streamlit_app.py
    Added Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 
-streamlit.title('my parents new healthy diner')
+streamlit.title('my parents new healthy diner 🥣 🥗 🐔 🥑🍞 ')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale , Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boild Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

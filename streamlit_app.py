48e442a1270204bc4a85795f26ce183fd66f7efa
nagendre1 <139125499+nagendre1@users.noreply.github.com>
2023-07-14 18:39:07 +0800

    remove unformatted json text

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ streamlit.dataframe(fruits_to_show)
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # Just writes the data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "apple")
+# streamlit.text(fruityvice_response.json()) # Just writes the data to the screen
 
 # Take the json test & normalize it 
 fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())

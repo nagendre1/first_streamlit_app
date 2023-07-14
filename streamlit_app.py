cdc81fa4654a620a52b671d09c164411936b3193
nagendre1 <139125499+nagendre1@users.noreply.github.com>
2023-07-14 20:31:13 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())
 #output on screen as a table
 streamlit.dataframe(fruityvice_normalize)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets['snowflake'])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
